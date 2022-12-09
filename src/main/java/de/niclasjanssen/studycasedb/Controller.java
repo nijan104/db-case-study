8f25772e0c5b08838e324bdfc8079fcad37277e7
Niclas Janssen <niclas.janssen@hhu.de>
2022-12-09 09:37:35 +0100

    added exception handling
    tried to add a way of handling the EntityNotFoundException without success

--- src/main/java/de/niclasjanssen/studycasedb/Controller.java
+++ src/main/java/de/niclasjanssen/studycasedb/Controller.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @RestController
 public class Controller {
 	
@@ -15,30 +17,51 @@ public class Controller {
 	
 	@GetMapping(value = "/api/v1/distance/{ds1}/{ds2}")
 	public Distance getDistance(@PathVariable String ds1, @PathVariable String ds2) {
-		
-		Station s1 = stationRepo.getReferenceById(ds1);
-		Station s2 = stationRepo.getReferenceById(ds2);
 		Distance d = new Distance();
-		d.setFrom(s1.getName());
-		d.setTo(s2.getName());
+		boolean exception = false;
 		
-		int distance;
+		try {
+		stationRepo.getReferenceById(ds1);
+		stationRepo.getReferenceById(ds2);
+		} catch (EntityNotFoundException e) {
+			exception = true;
+		}
 		
-		double dx = 111.3*Math.cos((s1.getBreite() + s2.getBreite())/2*(Math.PI/180))*(s1.getLaenge()-s2.getLaenge());
-		double dy = 111.3*(s1.getBreite()-s2.getBreite());
-		distance = (int) Math.sqrt(dx*dx + dy*dy);
-		d.setDistance(distance);
+		if (exception){
+			d.setFrom("Not both Stations could");
+			d.setTo("be found in database");		
+			return d;
+		}
+		else {
+			
+			Station s1 = stationRepo.getReferenceById(ds1);
+			Station s2 = stationRepo.getReferenceById(ds2);
+			d.setFrom(s1.getName());
+			d.setTo(s2.getName());
+		
+			int distance;
+		
+			double dx = 111.3*Math.cos((s1.getBreite() + s2.getBreite())/2*(Math.PI/180))*(s1.getLaenge()-s2.getLaenge());
+			double dy = 111.3*(s1.getBreite()-s2.getBreite());
+			distance = (int) Math.sqrt(dx*dx + dy*dy);
+			d.setDistance(distance);
 
 		return d;
+		}
+		
 	}
 	
 	@GetMapping(value = "/api/v1/distance/FV/{ds1}/{ds2}")
 	public Distance getFVDistance(@PathVariable String ds1, @PathVariable String ds2) {
-
-		if (stationRepo.getReferenceById(ds1).getVerkehr().equals("FV") && stationRepo.getReferenceById(ds2).getVerkehr().equals("FV")) {
-			return getDistance(ds1,ds2);
+		
+		Distance d = getDistance(ds1,ds2);
+		try {
+			if (stationRepo.getReferenceById(ds1).getVerkehr().equals("FV") && stationRepo.getReferenceById(ds2).getVerkehr().equals("FV")) {
+				return getDistance(ds1,ds2);
+			}
+		} catch (EntityNotFoundException e) {
+			
 		}
-		Distance d = new Distance();
 		d.setFrom("Not both Stations");
 		d.setTo("are included in FV");		
 		return d;
